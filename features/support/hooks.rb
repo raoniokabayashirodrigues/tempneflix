50ceec974a3b183ece2764c0efb47737884b85a1
Raoni Rodrigues <raoni.rodrigues@praxio.com.br>
2020-01-15 13:15:35 -0300

    Primeiro commit

---
+++ features/support/hooks.rb
@@ -0,0 +1,19 @@
+Before do
+  @login_page = LoginPage.new
+  @movie_page = MoviePage.new
+  @sidebar = SideBarView.new
+
+  page.current_window.resize_to(1920, 1080)
+end
+
+Before("@login") do
+  user = CONFIG["users"]["cat_manager"]
+  @login_page.go
+  @login_page.with(user["email"], user["pass"])
+  expect(page).to have_text user["name"]
+end
+
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
+end
